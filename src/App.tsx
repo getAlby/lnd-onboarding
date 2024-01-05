1189c1e15642b54a64b551a77f9c0004fad38b5d
Michael Bumann <hello@michaelbumann.com>
2024-01-05 17:07:52 +0100

    Ideas

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,60 @@ import lndImage from "./assets/lnd.png";
 import albyImage from "./assets/alby.png";
 import { getTransactionId, hexToBase64 } from "./utils";
 
-const MIN_BALANCE = 250_000;
+const MIN_BALANCE = 500_000;
 const RECOMMENDED_NODES: RecommendedChannel[] = [
   {
-    title: "alice",
+    title: "ACINQ",
     pubkey:
-      "02480dada93b7f44be452f245c63868eac33d55bd98db8f3bae0c6fbd2378d9ad1",
-    host: "alice:8080",
+      "03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f",
+    host: "3.33.236.230:9735",
+    min: 500000,
   },
   {
-    title: "bob",
+    title: "deezy prime ⚡ ✨",
     pubkey:
-      "035b8fcbe2072b5f92f7d420f5f43eeba17fa1d977e0acc2afff7ae4dca4cad136",
-    host: "bob:8082",
+      "0214ec5397050f7eec8e574d1d6feaa0c992169ed107056e6bd57aed1e94714851",
+    host: "54.159.16.197:9735",
+    min: 500000,
   },
+  {
+    title: "bitfinex.com",
+    pubkey:
+      "03cde60a6323f7122d5178255766e38114b4722ede08f7c9e0c5df9b912cc201d6",
+    host: "34.65.85.39:9745",
+    min: 500000,
+  },
+  {
+    title: "Breez",
+    pubkey:
+      "031015a7839468a3c266d662d5bb21ea4cea24226936e2864a7ca4f2c3939836e0",
+    host: "212.129.58.219:9735",
+    min: 10100000,
+  },
+  {
+    title: "kappa",
+    pubkey:
+      "0324ba2392e25bff76abd0b1f7e4b53b5f82aa53fddc3419b051b6c801db9e2247",
+    host: "54.69.203.247:9735",
+    min: 10000,
+    //min: 300000,
+  },
+  {
+    title: "Voltage.cloud (C2)",
+    pubkey: "02cfdc6b60e5931d174a342b20b50d6a2a17c6e4ef8e077ea54069a3541ad50eb0",
+    host: "52.89.237.109:9735",
+    min: 300000,
+  }
 ];
 
 type RecommendedChannel = {
   title: string;
   pubkey: string;
   host: string;
+  min: number;
 };
 
-const steps = ["Connect Node", "Fund Node", "Open Channel"];
+const steps = ["Connect Node", "Fund Your Node", "Open Channel"];
 
 function App() {
   const [currentStepIndex, setStepIndex] = React.useState(0);
@@ -35,11 +66,19 @@ function App() {
   const [balance, setBalance] = React.useState<
     { confirmed: number; unconfirmed: number } | undefined
   >();
+  const [alias, setAlias] = React.useState<string>();
   const [address, setAddress] = React.useState<string>();
   const [connectPubkey, setConnectPubkey] = React.useState<string>();
   const [connectHost, setConnectHost] = React.useState<string>();
+  const [channelOptions, setChannelOptions] =
+    React.useState<RecommendedChannel[]>([]);
   const [channelOpenTransactionId, setChannelOpenTransactionId] =
     React.useState<string>();
+
+  const MIN_BALANCE = Math.min(...RECOMMENDED_NODES.map((n) => n.min));
+  const findChannel = (amount: number) => {
+    return RECOMMENDED_NODES.filter((n) => amount > n.min);
+  };
 
   React.useEffect(() => {
     (async () => {
@@ -55,30 +94,43 @@ function App() {
         ) {
           return;
         }
+        setAlias(info.node?.alias);
         setStepIndex(1);
-        const balanceResponse = (await window.webln.request(
-          "walletbalance"
-        )) as {
-          confirmed_balance: string;
-          unconfirmed_balance: string;
-        };
-        setBalance({
-          confirmed: parseInt(balanceResponse.confirmed_balance),
-          unconfirmed: parseInt(balanceResponse.unconfirmed_balance),
-        });
-        if (parseInt(balanceResponse.confirmed_balance) >= MIN_BALANCE) {
+        const balanceResponse = await fetchBalance();
+        const confirmedBalance = parseInt(balanceResponse.confirmed_balance);
+        const channels = findChannel(confirmedBalance);
+        if (channels.length > 0) {
+          setChannelOptions(channels);
           setStepIndex(2);
         } else {
-          const newaddressResponse = (await window.webln.request(
-            "newaddress"
-          )) as { address: string };
-          setAddress(newaddressResponse.address);
+          const existingAddress = window.localStorage.getItem("address");
+          if (existingAddress) {
+            setAddress(existingAddress);
+          } else {
+            const newaddressResponse = (await window.webln.request(
+              "newaddress",
+            )) as { address: string };
+            setAddress(newaddressResponse.address);
+            window.localStorage.setItem("address", newaddressResponse.address);
+          }
         }
       }
       await init();
       setLoading(false);
     })();
   }, []);
+
+  async function fetchBalance() {
+    const balanceResponse = (await window.webln.request("walletbalance")) as {
+      confirmed_balance: string;
+      unconfirmed_balance: string;
+    };
+    setBalance({
+      confirmed: parseInt(balanceResponse.confirmed_balance),
+      unconfirmed: parseInt(balanceResponse.unconfirmed_balance),
+    });
+    return balanceResponse;
+  }
 
   async function openChannel() {
     if (!window.webln?.request) {
@@ -105,12 +157,11 @@ function App() {
     try {
       const openChannelResponse = (await window.webln.request("openchannel", {
         node_pubkey: hexToBase64(connectPubkey),
-        local_funding_amount: Math.floor(MIN_BALANCE * 0.9), // reserve enough balance for channel open fee
-        push_sat: 0,
+        fund_max: true,
       })) as { funding_txid_bytes: string };
       console.log("Open channel response", openChannelResponse);
       setChannelOpenTransactionId(
-        getTransactionId(openChannelResponse.funding_txid_bytes)
+        getTransactionId(openChannelResponse.funding_txid_bytes),
       );
     } catch (error) {
       console.error(error);
@@ -119,28 +170,42 @@ function App() {
   }
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <h1 className="mb-4 text-2xl text-center font-bold">Loading...</h1>;
   }
 
   return (
     <div
       data-theme="bumblebee"
-      className="flex justify-center items-start w-full min-h-full p-8 font-sans"
+      className="flex min-h-full w-full items-start justify-center p-8 font-sans"
     >
-      <div className="flex flex-col justify-center items-center max-w-lg w-full">
-        <h1 className="text-2xl mb-4 font-bold">LND Onboarding</h1>
-        <div className="flex justify-center items-center gap-4 mb-8">
-          <img src={albyImage} className="w-16 h-16 rounded" />
+      <div className="flex w-full max-w-lg flex-col items-center justify-center">
+        <h1 className="mb-4 text-2xl font-bold">Setup {alias}</h1>
+        <div className="mb-8 flex items-center justify-center gap-4">
+          <img src={albyImage} className="h-16 w-16 rounded" />
           <p>✗</p>
-          <img src={lndImage} className="w-16 h-16 rounded" />
+          <img src={lndImage} className="h-16 w-16 rounded" />
         </div>
-        <div className="flex flex-col justify-center items-center w-full shadow-lg rounded-2xl mb-8 p-4">
-          <h2 className="text-lg mb-8 font-semibold">
+        <div className="mb-8 flex w-full flex-col items-center justify-center rounded-2xl p-4 shadow-lg">
+          <h2 className="mb-8 text-lg font-semibold">
             {steps[currentStepIndex]}
           </h2>
           {currentStepIndex === 0 && (
             <>
-              <ol className="list-decimal list-inside">
+              <p>This wizard helps you to setup your new LND lightning node!</p>
+              <p>
+                In 3 steps it helps you to fund your node and open channels to
+                send and receive lightning payments.
+              </p>
+              <p>
+                Make sure you have the{" "}
+                <a href="https://getalby.com">Alby extension instaled</a> and
+                connected to a LND node.
+              </p>
+              <p>
+                To get a voltage cloud node you can{" "}
+                <a href="https://info.getalby.com/voltage">follow our guide</a>.
+              </p>
+              <ol className="list-inside list-decimal">
                 <li>Install the Alby extension</li>
                 <li>
                   Get your lightning node. We currently recommend Voltage cloud
@@ -148,7 +213,7 @@ function App() {
                 </li>
                 <li>Connect to your Lightning node from the Alby extension</li>
               </ol>
-              <p className="italic mt-4">
+              <p className="mt-4 italic">
                 Once the above steps are done, please refresh this page.
               </p>
             </>
@@ -156,13 +221,20 @@ function App() {
           {currentStepIndex === 1 && (
             <>
               <p>
-                Please send at least {MIN_BALANCE} sats to your node and wait
-                for onchain confirmation.
+                You currently have <strong>{balance?.confirmed} sats</strong> on
+                your onchain wallet.
               </p>
-              <p className="mt-4">Here's a deposit address:</p>
+              <p>
+                To open a open a lightning channel we recommend at{" "}
+                <strong>least {MIN_BALANCE} sats</strong>
+              </p>
+              <p>
+                Please send sats to your node and wait for one confirmation.
+              </p>
+              <p className="mt-4">Deposit address:</p>
               <input
                 readOnly
-                className="input input-bordered w-full text-center mb-4"
+                className="input input-bordered mb-4 w-full text-center"
                 value={address}
               />
 
@@ -172,10 +244,10 @@ function App() {
           )}
           {currentStepIndex === 2 && channelOpenTransactionId && (
             <>
-              <h3 className="font-semibold mb-4">Channel opening!</h3>
+              <h3 className="mb-4 font-semibold">Channel opening!</h3>
               <p className="mb-4">
-                Please wait for a few onchain confirmations and you'll be able
-                payments with Alby.
+                Please wait for at least <strong>3 confirmations</strong> and
+                you'll be able payments with the Alby Extension.
               </p>
               <a
                 className="link"
@@ -186,58 +258,73 @@ function App() {
               </a>
             </>
           )}
-          {currentStepIndex === 2 && !channelOpenTransactionId && (
-            <>
-              <p>Confirmed balance: {balance?.confirmed} sats</p>
-
-              <p className="mt-8 font-semibold mb-4">Recommended nodes</p>
-              <div className="flex flex-col gap-2">
-                {RECOMMENDED_NODES.map((node) => (
-                  <div
-                    key={node.pubkey}
-                    className="bg-neutral-100 w-full p-4 rounded"
-                  >
-                    <h2 className="card-title">{node.title}</h2>
-                    <div className="flex gap-4">
-                      <p className="break-all text-sm">{node.pubkey}</p>
-                      <div className="card-actions justify-end">
-                        <button
-                          onClick={() => {
-                            setConnectPubkey(node.pubkey);
-                            setConnectHost(node.host);
-                          }}
-                          className="btn btn-primary"
-                        >
-                          Select
-                        </button>
+          {currentStepIndex === 2 &&
+            !channelOpenTransactionId &&
+            channelOptions.length > 0 && (
+              <>
+                <p>
+                  Great, you have {balance?.confirmed} sats available to open
+                  a new lightning channel.
+                </p>
+                <p>
+                  This channel will allow you to <strong>send</strong>{" "}
+                  ligthning payments.
+                </p>
+                <p className="mb-4 mt-8 font-semibold">
+                  Open a channel with one of our recommended nodes
+                </p>
+                <div className="flex flex-col gap-2">
+                  {channelOptions.map((node) => (
+                    <div
+                      key={node.pubkey}
+                      className="w-full rounded bg-neutral-100 p-4"
+                    >
+                      <h2 className="card-title">{node.title}</h2>
+                      <div className="flex gap-4">
+                        <p className="break-all text-sm">{node.pubkey}</p>
+                        <div className="card-actions justify-end">
+                          <button
+                            onClick={() => {
+                              setConnectPubkey(node.pubkey);
+                              setConnectHost(node.host);
+                            }}
+                            className="btn btn-primary"
+                          >
+                            Select
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-              <div className="w-full">
-                <p className="mt-8">Connect to node</p>
-                <input
-                  className="input input-bordered w-full text-center mb-4"
-                  value={connectPubkey}
-                  onChange={(e) => setConnectPubkey(e.target.value)}
-                  placeholder="node pubkey"
-                />
-                <input
-                  className="input input-bordered w-full text-center mb-4"
-                  value={connectHost}
-                  onChange={(e) => setConnectHost(e.target.value)}
-                  placeholder="node host"
-                />
-                <button
-                  className="btn btn-primary btn-lg w-full"
-                  onClick={openChannel}
-                >
-                  Open Channel
-                </button>
-              </div>
-            </>
-          )}
+                  ))}
+                </div>
+                <div className="w-full">
+                  <p className="mt-8">Connect to node</p>
+                  <input
+                    className="input input-bordered mb-4 w-full text-center"
+                    value={connectPubkey}
+                    onChange={(e) => setConnectPubkey(e.target.value)}
+                    placeholder="node pubkey"
+                  />
+                  <input
+                    className="input input-bordered mb-4 w-full text-center"
+                    value={connectHost}
+                    onChange={(e) => setConnectHost(e.target.value)}
+                    placeholder="node host"
+                  />
+                  <button
+                    className="btn btn-primary btn-lg w-full"
+                    onClick={openChannel}
+                    disabled={!(connectHost && connectPubkey)}
+                  >
+                    Open Channel
+                  </button>
+                  <p>
+                    This will connect your node to the peer and open a new
+                    lightning channel with the maximum of your available balance.
+                  </p>
+                </div>
+              </>,
+              )}
         </div>
         <ul className="steps text-xs">
           {steps.map((step, index) => (
